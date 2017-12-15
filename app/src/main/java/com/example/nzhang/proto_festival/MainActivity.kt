02b9e35087aeceff36e84eac4de318ca23db4269
nathalie <mlle.zhang.nathalie@gmail.com>
2017-12-15 13:44:41 +0100

    feat(global) : bind event name in list view

--- app/src/main/java/com/example/nzhang/proto_festival/MainActivity.kt
+++ app/src/main/java/com/example/nzhang/proto_festival/MainActivity.kt
@@ -2,36 +2,70 @@ package com.example.nzhang.proto_festival
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.example.nzhang.proto_festival.model.Event
+import com.example.nzhang.proto_festival.model.Events
+import com.squareup.moshi.Moshi
+import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var recycleView: RecyclerView
+    lateinit private var recycleView: RecyclerView
+    lateinit private var divideItemDecoration: DividerItemDecoration
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.recycleView = findViewById(R.id.container_list)
-        this.recycleView.layoutManager = LinearLayoutManager(this)
-        this.recycleView.adapter = EventAdapter()
+        val mLayoutManager = LinearLayoutManager(this)
 
+        // Load and parse JSON
+        val eventsJson = loadJsonFromAssets()
+        Log.d("content", eventsJson)
+
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(Events::class.java)
+        val eventResponse = adapter.fromJson(eventsJson)
+
+        this.recycleView = findViewById(R.id.container_list)
+        this.recycleView.layoutManager = mLayoutManager
+        this.recycleView.adapter = EventAdapter(eventResponse!!.events)
+
+        this.divideItemDecoration = DividerItemDecoration(this.recycleView.context, mLayoutManager.orientation)
+        this.recycleView.addItemDecoration(divideItemDecoration)
+
+    }
+
+    private fun loadJsonFromAssets() : String {
+        val bytesStream: InputStream
+        try {
+            bytesStream = baseContext.assets.open("events.json")
+            if (bytesStream != null) {
+                val streamSize: Int = bytesStream.available() // Returns an estimate of the number of bytes that can be read
+                val buffer: ByteArray = kotlin.ByteArray(streamSize)
+                bytesStream.read(buffer)
+                bytesStream.close()
+                return String(buffer)
+            }
+        } catch (e: Exception) {
+            Log.e("error",  e.toString())
+        }
+        return ""
     }
 }
 
-class EventAdapter : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
-
-    private val events: List<Event> = listOf(
-            Event("Séance scolaire","Mutafukaz"),
-            Event("Atelier secret Fab", "Rencontre avec les réalisateurs")
-    )
+class EventAdapter(
+        private val events: List<Event>
+    ) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
     override fun getItemCount(): Int = events.size
 
@@ -44,12 +78,10 @@ class EventAdapter : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val event = events[position]
-        holder.categoryView.text = event.category
-        holder.titleView.text = event.title
+        holder.titleView.text = event.name
     }
 
     class EventViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val categoryView = view.findViewById<TextView>(R.id.text_list_item_category)
         val titleView = view.findViewById<TextView>(R.id.text_list_item_title)
 
     }
--- app/src/main/java/com/example/nzhang/proto_festival/model/Event.kt
+++ app/src/main/java/com/example/nzhang/proto_festival/model/Event.kt
@@ -4,6 +4,6 @@ package com.example.nzhang.proto_festival.model
  * Created by nathalie on 12/12/2017.
  */
 
-data class Event(val category: String, val title: String) {
+data class Event(val id: String, val name: String) {
 
 }
