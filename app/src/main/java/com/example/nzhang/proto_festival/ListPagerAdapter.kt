c9ea0ccaba079dd3e172af4ef09c8bcc0fc19880
nathalie <mlle.zhang.nathalie@gmail.com>
2018-02-22 19:41:54 +0100

    feat(public/pro) : add icon for tabLayout

--- app/src/main/java/com/example/nzhang/proto_festival/ListPagerAdapter.kt
+++ app/src/main/java/com/example/nzhang/proto_festival/ListPagerAdapter.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.content.res.AppCompatResources.getDrawable
+import android.text.Spanned
+import android.text.style.ImageSpan
+import android.text.SpannableStringBuilder
 
 class ListPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
@@ -23,11 +27,26 @@ class ListPagerAdapter(private val mContext: Context, fm: FragmentManager) : Fra
 
     // This determines the title for each tab
     override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            0 -> "Public"
-            1 -> "Professionnel"
-            else -> "Public"
+
+        val sb = SpannableStringBuilder(" ")
+
+        if (position == 0) {
+
+            val drawable = getDrawable(mContext, R.drawable.bouton_tp_clique)
+            drawable!!.setBounds(0, 0, 500, 165)
+            val imageSpan = ImageSpan(drawable)
+            //to make our tabs icon only, set the Text as blank string with white space
+            sb.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
+        if (position == 1) {
+            val drawable = getDrawable(mContext, R.drawable.bouton_pro)
+            drawable!!.setBounds(0, 0, 500, 165)
+            val imageSpan = ImageSpan(drawable)
+            //to make our tabs icon only, set the Text as blank string with white space
+            sb.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        return sb
     }
 
 }
--- app/src/main/java/com/example/nzhang/proto_festival/fragment/PlanningFragment.kt
+++ app/src/main/java/com/example/nzhang/proto_festival/fragment/PlanningFragment.kt
@@ -8,9 +8,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nzhang.proto_festival.*
+import android.support.v7.content.res.AppCompatResources
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ImageSpan
 
 
 class PlanningFragment : Fragment() {
+
+    lateinit private var tabLayout: TabLayout
+    private val tabunselected = arrayListOf(R.drawable.bouton_tp, R.drawable.bouton_pro)
+    private val tabselected = arrayListOf(R.drawable.bouton_tp_clique, R.drawable.bouton_pro_clique)
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -27,10 +35,39 @@ class PlanningFragment : Fragment() {
         viewPager.adapter = adapter
 
         // Give the TabLayout the ViewPager
-        val tabLayout: TabLayout = view.findViewById(R.id.planning_tabs)
+        tabLayout = view.findViewById(R.id.planning_tabs)
         tabLayout.setupWithViewPager(viewPager)
 
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                changeTabs(0, tab!!.position == 0)
+                changeTabs(1, tab.position == 1)
+            }
+        })
+
         return view
+    }
+
+    fun changeTabs(position: Int, selected: Boolean) {
+        if (selected){
+            val sb = SpannableStringBuilder(" ")
+            val drawable = AppCompatResources.getDrawable(context, tabselected[position])
+            drawable!!.setBounds(0, 0, 500, 165)
+            val imageSpan = ImageSpan(drawable)
+            sb.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            tabLayout.getTabAt(position)!!.text = sb
+        } else {
+            val sb = SpannableStringBuilder(" ")
+            val drawable = AppCompatResources.getDrawable(context, tabunselected[position])
+            drawable!!.setBounds(0, 0, 500, 165)
+            val imageSpan = ImageSpan(drawable)
+            sb.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            tabLayout.getTabAt(position)!!.text = sb
+        }
     }
 }
 
