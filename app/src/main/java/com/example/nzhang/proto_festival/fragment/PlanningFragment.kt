2e21593c2b485e0a1640479835e4e3e4e853f5cf
Melanie <melanie.ngo@edu.gobelins.fr>
2018-02-24 14:50:26 +0100

    feat(planning): add bottom sheet message to alert user of filter

--- app/src/main/java/com/example/nzhang/proto_festival/fragment/PlanningFragment.kt
+++ app/src/main/java/com/example/nzhang/proto_festival/fragment/PlanningFragment.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.support.constraint.ConstraintLayout
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.LinearSmoothScroller
@@ -14,6 +17,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.nzhang.proto_festival.*
 import com.example.nzhang.proto_festival.model.Categories
 import com.example.nzhang.proto_festival.model.Events
@@ -44,46 +48,70 @@ class PlanningFragment : Fragment(), DayAdapter.WhichDayClickedInterface {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_planning, container, false)
-
         recycleView = view.findViewById(R.id.day_list)
         val mLayoutManager = LinearLayoutManager(context)
         recycleView.layoutManager = mLayoutManager
         recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "")
 
-
+        val cover = view.findViewById<ConstraintLayout>(R.id.black_opacity_cover)
         val btnPro = view.findViewById<ImageButton>(R.id.btn_list_item_pro)
         val btnProContainer = view.findViewById<LinearLayout>(R.id.btn_container_pro)
         val btnPublic = view.findViewById<ImageButton>(R.id.btn_list_item_public)
         val btnPublicContainer = view.findViewById<LinearLayout>(R.id.btn_container_public)
+        val bottomSheet = view.findViewById<ConstraintLayout>(R.id.bottom_sheet)
+        val bottomSheetText = view.findViewById<TextView>(R.id.bottom_sheet_text)
+        val bottomBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        val handler = Handler()
+
         btnPro.setOnClickListener {
             btnPro.isSelected = !btnPro.isSelected
             btnPublic.isSelected = false
             btnPublic.setBackgroundResource(R.drawable.bouton_tp)
             btnPublicContainer.setBackgroundResource(R.drawable.borderlines)
+            cover.visibility = View.VISIBLE
+
             if (btnPro.isSelected) {
                 btnProContainer.setBackgroundResource(R.drawable.borderlines_full_green)
                 btnPro.setBackgroundResource(R.drawable.bouton_pro_clique)
                 recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "pro")
+                bottomSheetText.text = resources.getString(R.string.bottom_sheet_filter_pro)
             } else {
                 btnProContainer.setBackgroundResource(R.drawable.borderlines)
                 btnPro.setBackgroundResource(R.drawable.bouton_pro)
                 recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "")
+                bottomSheetText.text = resources.getString(R.string.bottom_sheet_no_filter)
             }
+            bottomBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            handler.postDelayed({
+                bottomBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                cover.visibility = View.GONE
+            }, 4000)
         }
         btnPublic.setOnClickListener {
             btnPublic.isSelected = !btnPublic.isSelected
             btnPro.isSelected = false
             btnPro.setBackgroundResource(R.drawable.bouton_pro)
             btnProContainer.setBackgroundResource(R.drawable.borderlines)
-            if (btnPublic.isSelected) {
-                btnPublicContainer.setBackgroundResource(R.drawable.borderlines_full_green)
-                btnPublic.setBackgroundResource(R.drawable.bouton_tp_clique)
-                recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "public")
-            } else {
-                btnPublicContainer.setBackgroundResource(R.drawable.borderlines)
-                btnPublic.setBackgroundResource(R.drawable.bouton_tp)
-                recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "")
-            }
+            cover.visibility = View.VISIBLE
+
+                if (btnPublic.isSelected) {
+                    btnPublicContainer.setBackgroundResource(R.drawable.borderlines_full_green)
+                    btnPublic.setBackgroundResource(R.drawable.bouton_tp_clique)
+                    recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "public")
+                    bottomSheetText.text = resources.getString(R.string.bottom_sheet_filter_public)
+                } else {
+                    btnPublicContainer.setBackgroundResource(R.drawable.borderlines)
+                    btnPublic.setBackgroundResource(R.drawable.bouton_tp)
+                    recycleView.adapter = DayAdapter(finalItemsList, places, categories, this, "")
+                    bottomSheetText.text = resources.getString(R.string.bottom_sheet_no_filter)
+                }
+            bottomBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            handler.postDelayed({
+                bottomBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                cover.visibility = View.GONE
+            }, 4000)
         }
 
 
